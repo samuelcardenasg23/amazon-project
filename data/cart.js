d1a06e08211ca6c3db41f229b8347a593cafdb9f
Samuel Cardenas <samuelcardenas1706@gmail.com>
2023-12-23 15:33:26 -0500

    Fix price formatting and radio selectors

--- data/cart.js
+++ data/cart.js
@@ -5,10 +5,6 @@ export const cart = [{
 {
     productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
     quantity: 1
-},
-{
-    productId: '3ebe75dc-64d2-4137-8860-1f5a963e534b',
-    quantity: 20
 }];
 
 export function addToCart(productId) {
