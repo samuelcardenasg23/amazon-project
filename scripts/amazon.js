3d45310da8cccfb01fdf51e42648929439f2f140
Samuel Cardenas <samuelcardenas1706@gmail.com>
2023-12-26 15:56:27 -0500

    Added to cart animation works now

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -45,7 +45,7 @@ products.forEach((product) => {
 
                 <div class="product-spacer"></div>
 
-                <div class="added-to-cart">
+                <div class="added-to-cart js-added-to-cart-${product.id}">
                     <img src="images/icons/checkmark.png">
                     Added
                 </div>
@@ -67,11 +67,30 @@ function updateCartQuantity() {
     document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
 }
 
+// const addedMessage = document.querySelector(
+//     `.js-added-to-cart-${productId}`
+// );
+
+// addedMessage.classList.add('added-to-cart-visible');
+
+// setTimeout(() => {
+//     addedMessage.classList.remove('added-to-cart-visible');
+// }, 2000);
+
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
     button.addEventListener('click', () => {
         const productId = button.dataset.productId;
+        const addedMessage = document.querySelector(
+            `.js-added-to-cart-${productId}`
+        );
+        
+        addedMessage.classList.add('added-to-cart-visible');
+        
+        setTimeout(() => {
+            addedMessage.classList.remove('added-to-cart-visible');
+        }, 2000);
         addToCart(productId);
         updateCartQuantity();
     });
