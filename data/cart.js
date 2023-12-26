e235336fd170f463c37a64a96d57119110cd506c
Samuel Cardenas <samuelcardenas1706@gmail.com>
2023-12-26 15:34:03 -0500

    Quantity selector to addToCart is now working

--- data/cart.js
+++ data/cart.js
@@ -35,12 +35,18 @@ export function addToCart(productId) {
         }
     });
 
+    const quantitySelector = document.querySelector(
+        `.js-quantity-selector-${productId}`
+    );
+
+    const quantity = Number(quantitySelector.value);
+
     if (matchingItem) {
-        matchingItem.quantity += 1;
+        matchingItem.quantity += quantity;
     } else {
         cart.push({
             productId: productId,
-            quantity: 1,
+            quantity: quantity,
             deliveryOptionId: '1'
         });
     }
