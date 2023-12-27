75f74fc66dafaef56a8df475fe6745365ed1afff
Samuel Cardenas <samuelcardenas1706@gmail.com>
2023-12-27 09:10:27 -0500

    Checkout quantity now responsive in the checkout page

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,7 @@
+import { renderCheckoutHeader } from "./ckeckout/checkoutHeader.js";
 import { renderOrderSummary } from "./ckeckout/orderSummary.js";
 import { renderPaymentSummary } from "./ckeckout/paymentSummary.js";
 
+renderCheckoutHeader();
 renderOrderSummary();
 renderPaymentSummary();
